fe706860c05af7971f915b7e8550c0f081e53b5d
Jaik <jaikli115@gmail.com>
2020-08-31 10:16:01 +0500

    this constructor

--- js/script.js
+++ js/script.js
@@ -1,42 +1,85 @@
-window.addEventListener('DOMContentLoaded', () =>{
+'use strict';
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-          
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show');
-        });
+// function show(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
+//     console.log(sum());
+// }
 
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
+// show(4, 3);
 
-    }
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this);
+//         }
+//        shout();
+//     }
+// };
 
-    hideTabContent();
-    showTabContent();
-    
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
+// obj.sum();
 
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`Hello ` + this.name);
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: "Jhon"
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+
+// console.log(double(2));
+// console.log(double(13));
+
+// 1) Обычная функция: this = window, но если use strict = undifiend
+// 2) Контекст у методов обьекта - сам обьект
+// 3) this в конструкторах и классах - это новый экземпляр обьекта
+// 4) Ручная привязка this: call, apply, bind
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function () {
+    this.style.backgroundColor = 'red';
+    console.log(this);
 });
 
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this);
+        };
+        say();
+    }
+};
 
+obj.sayNumber();
+
+const double = a =>  a * 2;
+
+console.log(double(4));
